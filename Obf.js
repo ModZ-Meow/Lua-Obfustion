df17a078caff5fd1484d6689f5b8513a47449a12
ModZ-Meow <tronghoduc11@gmail.com>
2026-05-28 17:03:58 +0700

    Create Obf.js

---
+++ Obf.js
@@ -0,0 +1,25 @@
+function obfuscate(code) {
+
+    let b = Buffer.from(code);
+
+    let key = 0x55;
+    let out = [];
+
+    for (let i = 0; i < b.length; i++) {
+
+        key ^= (i + 1);
+        out.push(b[i] ^ key);
+    }
+
+    return `
+-- OBF BOT OUTPUT
+
+local data = {${out.join(",")}}
+
+local key = 0x55
+
+print("loaded obf")
+`;
+}
+
+module.exports = { obfuscate };
