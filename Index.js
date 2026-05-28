13f7a155f836a6d7cd1581b9fba74cce34503912
ModZ-Meow <tronghoduc11@gmail.com>
2026-05-28 17:03:23 +0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,42 @@
+const { Client, GatewayIntentBits, AttachmentBuilder } = require("discord.js");
+const fs = require("fs");
+const { obfuscate } = require("./obf");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on("messageCreate", async (msg) => {
+
+    if (msg.author.bot) return;
+
+    if (!msg.content.startsWith("/obf")) return;
+
+    let code = msg.content.slice(4).trim();
+
+    if (!code) {
+        return msg.reply("Paste code");
+    }
+
+    let loading = await msg.reply("please wait...");
+
+    let result = obfuscate(code);
+
+    let file = "./obf.lua";
+    fs.writeFileSync(file, result);
+
+    let attachment = new AttachmentBuilder(file);
+
+    await msg.reply({
+        content: `<@${msg.author.id}>`,
+        files: [attachment]
+    });
+
+    loading.delete().catch(()=>{});
+});
+
+client.login("YOUR_BOT_TOKEN");
